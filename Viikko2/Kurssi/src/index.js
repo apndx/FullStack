9673de7c038a6261e105fed3cb3e470f2c0828de
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2018-12-05 12:15:14 +0200

    2.5 erillinen moduuli

--- Viikko2/Kurssi/src/index.js
+++ Viikko2/Kurssi/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Kurssi from './components/Kurssi'
 
 const App = () => {
 
@@ -43,62 +44,11 @@ const App = () => {
         }
       ]
     return (
-        <ul>
-          {kurssit.map(kurssi => <Kurssi key ={kurssi.id}kurssi={kurssi} />)}
-        </ul>
-    )
-}
-
-const Kurssi = ({kurssi}) => {
-
-    console.log('kurssin saama props', kurssi)
-    return (
-
-        <li>
-            <div>            
-            <Otsikko  otsikko ={kurssi} />     
-            </div>
-            <div>
-                <Sisalto sisalto ={kurssi.osat}/>       
-            </div>
-            <div>
-                <Yhteensa yhteensa ={kurssi.osat}/>  
-            </div>
-        </li>
-    )
-}
-
-const Otsikko = ({otsikko}) => {
-    console.log('otsikon saama props', otsikko)
-    return (
         <div>
-           <h1> {otsikko.nimi}</h1>
-        </div>
-    )
-}
-
-const Sisalto = (osat) => {
-    
-    console.log('sisällön saama props', osat)
-    return (  
-        <ul>
-        {osat.sisalto.map(osa=> <Osa key={osa.id} osa={osa}/>)}        
-        </ul>
-    )
-}
-
-const Osa = ({osa}) => {
-    console.log('osan saama props', osa)
-    return (
-        <li>{osa.nimi} {osa.tehtavia}</li>
-    )
-}
-
-const Yhteensa = (osat) => {
-    const yht = osat.yhteensa.reduce((acc, osa) => acc + osa.tehtavia, 0);
-    return (
-        <div>
-           <p>tehtäviä {yht} yhteensä </p>
+            <h1>Opetusohjelma</h1>      
+            <ul>                   
+                {kurssit.map(kurssi => <Kurssi key ={kurssi.id}kurssi={kurssi} />)}
+            </ul>
         </div>
     )
 }
