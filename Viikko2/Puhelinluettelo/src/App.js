4b0febbb591ecc27cd2e0f4b10a95ae7604f03a4
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2018-12-05 15:00:33 +0200

    2.6 puhelinluettelo osa 1

---
+++ Viikko2/Puhelinluettelo/src/App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: props.persons,
+      newName: ''
+    }
+  }
+
+  addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: this.state.newName,
+      id: this.state.persons.length + 1
+    }
+  
+    const persons = this.state.persons.concat(personObject)
+  
+    this.setState({
+      persons,
+      newName: ''
+    })
+  }
+
+  handlePersonChange = (event) => {
+    console.log(event.target.value)
+    this.setState({ newName: event.target.value })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form onSubmit={this.addPerson}>     
+            nimi: 
+          <input 
+            value = {this.state.newName}
+            onChange = {this.handlePersonChange}
+          />
+         <button type="submit">lisää</button>
+        </form>
+        <h2>Numerot</h2>
+        <ul>
+          {this.state.persons.map(person => <Person key={person.id} person={person} />)}
+        </ul>       
+      </div>
+    )
+  }
+}
+
+
+const Person = ({ person}) => {
+
+  console.log(person)
+  console.log(person.name)
+  return (
+    <li> {person.name}</li>
+  )
+}
+
+export default App
