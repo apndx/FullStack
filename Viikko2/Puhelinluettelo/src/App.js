f4053e5c32cbd3453d316b2b501360927ed6253e
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2018-12-09 15:24:33 +0200

    2.11 puhelinluettelo osa 6

--- Viikko2/Puhelinluettelo/src/App.js
+++ Viikko2/Puhelinluettelo/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import axios from 'axios'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      persons: props.persons,
+      persons: [],
       newName: '',
       newPhone: ''
     }
+    console.log('constructor')
   }
 
   addName = (event) => {
@@ -60,7 +62,18 @@ class App extends React.Component {
     }  
   }
 
+  componentDidMount() {
+    console.log('did mount')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        this.setState({ persons: response.data })
+      })
+  }
+
   render() {
+    console.log('render')
     return (
       <div>
         <h2>Puhelinluettelo</h2>
