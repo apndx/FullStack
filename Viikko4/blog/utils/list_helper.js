afa3c2215c293a07ecdcff4a58632ad01b732179
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2019-01-05 16:15:57 +0200

    4.4 apufunktioita ja yksikkötestejä, osa 2

--- Viikko4/blog/utils/list_helper.js
+++ Viikko4/blog/utils/list_helper.js
@@ -1,7 +1,17 @@
 const dummy = (blogs) => {
     return 1;
-  }
-  
+}
+
+const totalLikes = (blogs) => {
+
+    const likes = blogs.map(blog => blog.likes)
+
+    const reducer = (sum, item) => {
+        return sum + item
+    }
+    return likes.reduce(reducer, 0)
+}
+
   module.exports = {
-    dummy
+    dummy, totalLikes
   }
