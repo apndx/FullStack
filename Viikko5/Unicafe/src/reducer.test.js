3343ab620e3286d57e33d14d050605ee46de6e59
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2019-02-18 07:47:01 +0200

    5.17 unicafe revisited, osa 1 -lisää testejä

--- Viikko5/Unicafe/src/reducer.test.js
+++ Viikko5/Unicafe/src/reducer.test.js
@@ -32,4 +32,65 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+
+  it('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+
+  it('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  it('many changes are remembered', () => {
+    const action1 = {
+      type: 'OK'
+    }
+
+    const action2 = {
+      type: 'GOOD'
+    }
+
+    const action3 = {
+      type: 'GOOD'
+    }
+
+    const state = initialState
+
+    deepFreeze(state)
+    const newState1 = counterReducer(state, action1)
+    const newState2 = counterReducer(newState1, action2)
+    const newState3 = counterReducer(newState2, action3)
+    expect(newState3).toEqual({
+      good: 2,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+
+
 })
