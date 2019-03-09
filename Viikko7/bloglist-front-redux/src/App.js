abf1fd379c40657641047bb4c63005a0ab85e3f7
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2019-03-09 18:32:26 +0200

    7.5 redux, step2

--- Viikko7/bloglist-front-redux/src/App.js
+++ Viikko7/bloglist-front-redux/src/App.js
@@ -9,6 +9,9 @@ import BlogForm from './components/BlogForm'
 import  { useField } from './hooks'
 import { connect } from 'react-redux'
 import { changeNotification } from './reducers/notificationReducer'
+import { initializeBlogs } from './reducers/blogReducer'
+import { likeBlog } from './reducers/blogReducer'
+import { deleteBlog } from './reducers/blogReducer'
 
 const App = ( props ) => {
   const [blogs, setBlogs] = useState([])
@@ -24,10 +27,9 @@ const App = ( props ) => {
   const blogFormRef = React.createRef()
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs.sort(function(a,b) {return b.likes-a.likes}) )
-    )
+    props.initializeBlogs()
   }, [])
+
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
@@ -60,52 +62,38 @@ const App = ( props ) => {
     }
   }
 
-  const deleteBlog = async id => {
-    console.log('deleteblogin props', props)
-    console.log('deleteblogin saama id', id)
+  // const deleteBlog = async id => {
+  //   console.log('deleteblogin props', props)
+  //   console.log('deleteblogin saama id', id)
 
-    const blog = blogs.find(n => n.id ===id)
-    console.log('deleteblogin hakema blog', blog)
+  //   const blog = blogs.find(n => n.id ===id)
+  //   console.log('deleteblogin hakema blog', blog)
 
-    if (window.confirm('Do you wish to delete this blog?')) {
+  //   if (window.confirm('Do you wish to delete this blog?')) {
 
-      try {
-        const answer = await blogService.del(id)
-        console.log('deleteblogin saama answer', answer)
-        setBlogs(blogs.filter(b => b.id !==id))
-        //setNotification(`' ${answer.title} ' blog has now been deleted`)
-        props.changeNotification(`' ${blog.title} ' blog has now been deleted`, 5)
+  //     try {
+  //       const answer = await blogService.del(id)
+  //       console.log('deleteblogin saama answer', answer)
+  //       setBlogs(blogs.filter(b => b.id !==id))
+  //       //setNotification(`' ${answer.title} ' blog has now been deleted`)
+  //       props.changeNotification(`' ${blog.title} ' blog has now been deleted`, 5)
 
-      } catch(exception) {
-        console.log('deleteblogin catchaama exeption', exception)
-        props.changeNotification(`The blog '${blog.title}' is not added by you and can't be deleted`, 5)
-      }
-    }
+  //     } catch(exception) {
+  //       console.log('deleteblogin catchaama exeption', exception)
+  //       props.changeNotification(`The blog '${blog.title}' is not added by you and can't be deleted`, 5)
+  //     }
+  //   }
+  // }
+
+  const like = (id, title) => {
+    props.likeBlog(id)
+    props.changeNotification(`'${title}' has now been liked`, 5)
   }
 
-  const likeBlog = id => {
-
-    const blog = blogs.find(n => n.id ===id)
-    const changedBlog = {
-      title: blog.title,
-      author: blog.author,
-      url: blog.url,
-      likes: blog.likes+1,
-      user: blog.user,
-    }
-
-    console.log('likeBlogin blog', blog)
-    console.log('likeBlogin changedBlog', changedBlog)
-
-    blogService
-      .update(id, changedBlog)
-      .then(returned => {
-        setBlogs(blogs.map(blog => blog.id !== id ? blog : returned).sort(function(a,b) {return b.likes-a.likes}))
-      })
-      .catch(error => {
-        props.changeNotification(`The blog '${blog.title}' has already been deleted from the server`, 5)
-        setBlogs(blogs.filter(n => n.id !== id).sort(function(a,b) {return b.likes-a.likes}))
-      })
+  const delBlog = (id, title) => {
+    props.deleteBlog(id)
+    props.changeNotification(`' ${title}' blog has now been deleted`, 5)
+    props.initializeBlogs()
   }
 
   const logout = async (event) => {
@@ -183,6 +171,7 @@ const App = ( props ) => {
       setAuthor('')
       setUrl('')
       props.changeNotification(` '${blogObject.title}' blog added`, 5)
+      props.initializeBlogs()
     }
   }
 
@@ -205,12 +194,12 @@ const App = ( props ) => {
       <button onClick={logout}>logout</button>
       {blogForm()}
       <h2>BlogList</h2>
-      {blogs.map(blog =>
+      {props.blogs.map(blog =>
         <Blog
           key={blog.id}
           blog={blog}
-          onLike= {() => likeBlog(blog.id)}
-          onDelete = {() => deleteBlog(blog.id)}
+          onLike= {() => like(blog.id, blog.title)}
+          onDelete = {() => delBlog(blog.id, blog.title)}
         />
       )}
     </div>
@@ -219,12 +208,16 @@ const App = ( props ) => {
 
 const mapStateToProps = (state) => {
   return {
-    notification: state.notification
+    notification: state.notification,
+    blogs: state.blogs
   }
 }
 
 const mapDispatchToProps = {
-  changeNotification
+  changeNotification,
+  initializeBlogs,
+  likeBlog,
+  deleteBlog
 }
 
 
---
+++ Viikko7/bloglist-front-redux/src/reducers/blogReducer.js
@@ -0,0 +1,76 @@
+import blogService from '../services/blogs'
+//import notificationReducer from './notificationReducer'
+
+const blogReducer = (state = [], action) => {
+  console.log('blogreducerin saama action', action)
+  console.log('blogreducerin saama state', state)
+  switch(action.type) {
+  case 'LIKE':
+    const id = action.data.id
+    const blogToChange = state.find(n => n.id ===id)
+    const changedBlog = { ...blogToChange, likes: (blogToChange.likes+1) }
+    return state.map(blog => blog.id !== id ? blog : changedBlog ).sort(function(a,b) {return b.likes-a.likes})
+  case 'DELETE':
+    const delId = action.data.id
+    return state.filter(n => n.id !== delId).sort(function(a,b) {return b.likes-a.likes})
+  case 'INIT_BLOGS':
+    return action.data.sort(function(a,b) {return b.likes-a.likes})
+  default:
+    return state
+  }
+}
+
+export const initializeBlogs = () => {
+  return async dispatch => {
+    const anecdotes = await blogService.getAll()
+    dispatch({
+      type: 'INIT_BLOGS',
+      data: anecdotes
+    })
+  }
+}
+
+export const likeBlog = (id) => {
+
+  return async dispatch => {
+    const blogs = await blogService.getAll()
+    const blogToLike = blogs.find(n => n.id === id)
+    const likedBlog = { ...blogToLike, likes: (blogToLike.likes+1) }
+    //try {
+    await blogService.update(id, likedBlog)
+    dispatch({
+      type: 'LIKE',
+      data: { id }
+    })
+    //} catch (exception) {
+    //  state.changeNotification(`The blog '${blogToLike.title}' has already been deleted from the server`, 5)
+    //  initializeBlogs(blogs.filter(n => n.id !== id).sort(function(a,b) {return b.likes-a.likes}))
+    //}
+  }
+}
+
+
+export const deleteBlog = (id) => {
+
+  return async dispatch => {
+    //const blogs = await blogService.getAll()
+    if (window.confirm('Do you wish to delete this blog?')) {
+      //try {
+      const answer = await blogService.del(id)
+      console.log('deleteblogin saama answer', answer)
+      //initializeBlogs(blogs.filter(b => b.id !==id))
+      dispatch({
+        type: 'DELETE',
+        data: { id }
+      })
+      // } catch(exception) {
+      //   console.log('deleteblogin catchaama exeption', exception)
+      //   notificationReducer.changeNotification(`The blog '${blogToDelete.title}' is not added by you and can't be deleted`, 5)
+      // }
+    }
+  }
+}
+
+export default blogReducer
+
+
