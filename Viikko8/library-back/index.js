9c187ec4031515ce60949ec396884c5656b5ddc8
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2020-01-02 22:06:47 +0200

    8.10: Kirjan lisäys

--- Viikko8/library-back/index.js
+++ Viikko8/library-back/index.js
@@ -3,30 +3,30 @@ const uuid = require('uuid/v1')
 
 let authors = [
   {
-    name: 'Robert Martin',
+    authorName: 'Robert Martin',
     id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
     born: 1952,
     bookCount: 2
   },
   {
-    name: 'Martin Fowler',
+    authorName: 'Martin Fowler',
     id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
     born: 1963,
     bookCount: 1
   },
   {
-    name: 'Fyodor Dostoevsky',
+    authorName: 'Fyodor Dostoevsky',
     id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
     born: 1821,
     bookCount: 2
   },
   { 
-    name: 'Joshua Kerievsky', // birthyear not known
+    authorName: 'Joshua Kerievsky', // birthyear not known
     id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
     bookCount: 1
   },
   { 
-    name: 'Sandi Metz', // birthyear not known
+    authorName: 'Sandi Metz', // birthyear not known
     id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
     bookCount: 1
   },
@@ -42,49 +42,49 @@ let books = [
   {
     title: 'Clean Code',
     published: 2008,
-    author: 'Robert Martin',
+    authorName: 'Robert Martin',
     id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
     genres: ['refactoring']
   },
   {
     title: 'Agile software development',
     published: 2002,
-    author: 'Robert Martin',
+    authorName: 'Robert Martin',
     id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
     genres: ['agile', 'patterns', 'design']
   },
   {
     title: 'Refactoring, edition 2',
     published: 2018,
-    author: 'Martin Fowler',
+    authorName: 'Martin Fowler',
     id: "afa5de00-344d-11e9-a414-719c6709cf3e",
     genres: ['refactoring']
   },
   {
     title: 'Refactoring to patterns',
     published: 2008,
-    author: 'Joshua Kerievsky',
+    authorName: 'Joshua Kerievsky',
     id: "afa5de01-344d-11e9-a414-719c6709cf3e",
     genres: ['refactoring', 'patterns']
   },  
   {
     title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
     published: 2012,
-    author: 'Sandi Metz',
+    authorName: 'Sandi Metz',
     id: "afa5de02-344d-11e9-a414-719c6709cf3e",
     genres: ['refactoring', 'design']
   },
   {
     title: 'Crime and punishment',
     published: 1866,
-    author: 'Fyodor Dostoevsky',
+    authorName: 'Fyodor Dostoevsky',
     id: "afa5de03-344d-11e9-a414-719c6709cf3e",
     genres: ['classic', 'crime']
   },
   {
     title: 'The Demon',
     published: 1872,
-    author: 'Fyodor Dostoevsky',
+    authorName: 'Fyodor Dostoevsky',
     id: "afa5de04-344d-11e9-a414-719c6709cf3e",
     genres: ['classic', 'revolution']
   },
@@ -93,13 +93,13 @@ let books = [
 const typeDefs = gql`
   type Book {
     title: String!
-    author: String!
+    authorName: String!
     published: String!
     genres: [String!]!
   }
 
   type Author {
-    name: String
+    authorName: String
     id: ID
     born: Int
     bookCount: Int
@@ -109,28 +109,27 @@ const typeDefs = gql`
     bookCount: Int!
     authorCount: Int!
     allBooks(author: String, genre: String): [Book]
-    findAuthor(name: String!): Author
+    findAuthor(authorName: String!): Author
     allAuthors: [Author!]!
   }
 
   type Mutation {
     addAuthor(
-      name: String!
-      id: ID!
+      authorName: String!
       born: Int
       bookCount: Int
     ): Author,
     addToAuthorBookCount(
-      name: String!
+      authorName: String!
       bookCount: Int
       ): Author,
     editAuthorBorn(
-      name: String
+      authorName: String
       setBornTo: Int
     ): Author, 
     addBook(
       title: String!
-      author: String!
+      authorName: String!
       published: Int!
       genres: [String!]!
     ): Book
@@ -143,7 +142,7 @@ const resolvers = {
     authorCount: () => authors.length,
     allBooks: (root, args) => {
       if (args.author) {
-        return books.filter(book => book.author === args.author)      
+        return books.filter(book => book.authorName === args.authorName)      
       } else if (args.genre) {
         return books.filter(book => book.genres.includes(args.genre))
       } else {
@@ -151,20 +150,21 @@ const resolvers = {
       }
     },  
     findAuthor: (root, args) =>
-      authors.find(author => author.name === args.name),
+      authors.find(author => author.authorName === args.authorName),
     allAuthors: () => authors
   },
   Mutation: {
     addBook: (root, args) => {
       const book = { ...args, id: uuid() }
       books = books.concat(book)
-      const existingAuthor = authors.find(a => a.name === args.author)
+      const existingAuthor = authors.find(a => a.authorName === args.authorName)
       if (existingAuthor) {
         const authorHasOneMoreBook = {...existingAuthor, bookCount: existingAuthor.bookCount+1 }
-        authors = authors.map(author => author.name !== existingAuthor.name ? author : authorHasOneMoreBook)
+        authors = authors.map(author => author.authorName !== existingAuthor.authorName ? author : authorHasOneMoreBook)
         return book
       } else {
         const author = { ...args, id: uuid(), bookCount: 1 }
+        console.log('ADDBOOK AUTHOR', author)
         authors = authors.concat(author)
         return book
       }  
@@ -175,12 +175,12 @@ const resolvers = {
       return author
     },
     addToAuthorBookCount: (root, args) => {
-      const author = authors.find(author => author.name === args.name)
+      const author = authors.find(author => author.authorName === args.authorName)
       const authorHasOneMoreBook = {...author, bookCount: author.bookCount+1 }
       return authorHasOneMoreBook
     },
     editAuthorBorn: (root, args) => {
-      const author = authors.find(author => author.name === args.name)
+      const author = authors.find(author => author.authorName === args.authorName)
       if (author) {
         return authorWithBorn = {...author, born: args.setBornTo }
       } 
