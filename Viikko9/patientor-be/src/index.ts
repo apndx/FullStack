e19b84386e8ba1eb86ca5ca9563fde060394d0d6
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2021-05-30 14:36:46 +0300

    Add diagnose route and service, 9.10: Patientor backend, step3

--- Viikko9/patientor-be/src/index.ts
+++ Viikko9/patientor-be/src/index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
+import diagnoseRouter from './routes/diagnoses';
+
 const app = express();
 app.use(express.json());
-
 const cors = require('cors')
-
 app.use(cors())
 
 const PORT = 3001;
@@ -12,6 +12,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
